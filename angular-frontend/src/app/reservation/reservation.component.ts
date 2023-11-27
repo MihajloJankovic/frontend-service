5d633d29617a3642ec0e3e40ad06028c9b2363da
milosavljevicstefan <milosavljevic@skiff.com>
2023-11-27 12:32:02 +0100

    zavrsena implementacija za access point

--- angular-frontend/src/app/reservation/reservation.component.ts
+++ angular-frontend/src/app/reservation/reservation.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { AuthGuard } from '../services/auth.guard';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-reservation',
@@ -13,7 +15,9 @@ export class ReservationComponent {
   constructor(
     private fb: FormBuilder,
     public dialogRef: MatDialogRef<ReservationComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private authGuard: AuthGuard,
+    private router: Router
   ) {
     this.reservationForm = this.fb.group({
       name: ['', Validators.required],
@@ -21,6 +25,21 @@ export class ReservationComponent {
       checkIn: ['', Validators.required],
       checkOut: ['', Validators.required]
     });
+  }
+
+  ngOnInit(): void {
+    // Perform role check
+    const canActivate = this.authGuard.canActivate(
+      {} as ActivatedRouteSnapshot,
+      {} as RouterStateSnapshot
+    );
+
+    if (!canActivate) {
+      console.log('Unauthorized access');
+      this.router.navigate(['/login']);
+    } else {
+      console.log('Component initialized');
+    }
   }
 
   submitReservation() {
---
+++ angular-frontend/src/app/services/auth.guard.ts
@@ -0,0 +1,34 @@
+// auth.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private jwtHelper: JwtHelperService) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const token = localStorage.getItem('jwt');
+
+    if (!token || this.jwtHelper.isTokenExpired(token)) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+    if ('roles' in route.data && !this.checkRoles(token)) {
+      this.router.navigate(['/login']);
+      return false;
+    }
+
+    return true;
+  }
+
+  private checkRoles(token: string): boolean {
+    const decodedToken = this.jwtHelper.decodeToken(token);
+    return !!decodedToken.role;
+  }
+  
+}
